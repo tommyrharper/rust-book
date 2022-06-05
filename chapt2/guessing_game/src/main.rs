6ea2f01756b4c3a84899cfcc4373abbbed80d975
tommyrharper <thomasrobertharper@gmail.com>
2022-06-05 18:55:30 +0100

    feat: guesing loop with break

--- chapt2/guessing_game/src/main.rs
+++ chapt2/guessing_game/src/main.rs
@@ -3,32 +3,31 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+  println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("The secret number is: {}", secret_number);
+  let secret_number = rand::thread_rng().gen_range(1..101);
+  println!("The secret number is: {}", secret_number);
 
+  loop {
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+      .read_line(&mut guess)
+      .expect("Failed to read line");
 
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+      Ordering::Less => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => {
+        println!("You win!");
+        break;
+      }
     }
-
-    // if secret_number == guess_int {
-    //     println!("You win!");
-    // } else {
-    //     println!("You lose!");
-    // }
+  }
 }
