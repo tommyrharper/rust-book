04e40cd44dacdbd81db79fcbee7bf3763888096f
tommyrharper <thomasrobertharper@gmail.com>
2022-06-05 19:32:00 +0100

    feat: shadowing

--- chapt3/README.md
+++ chapt3/README.md
@@ -1,3 +1,15 @@
 # Chapt 3
 
+## Constants
+
+- Can be in global scope
+- Cannot be prefixed with `mut`
+- Type must be declared
+- Can only be set to a constant expression, no the result ofa function call or anything that could be determined at runtime
+
+## Shadowing
+
+- Updates the value assigned to a variable within the block scope of the "shadow".
+- Reassigning as opposed to shadowing, will change the value of a variable within the variables original scope.
+
 
--- chapt3/variables/src/main.rs
+++ chapt3/variables/src/main.rs
@@ -1,3 +1,22 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
-    println!("Hello, world!");
+    // Mutation
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    println!("{}", THREE_HOURS_IN_SECONDS);
+
+    // Shadowing
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        // x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
 }
